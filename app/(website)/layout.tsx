acfea84f0c7b9f2d978eed1f2a91011464733a8e
Muhammad Amir <amirawa99925@gmail.com>
2026-01-11 23:59:44 +0500

    setup folder structure and added basic layour for website, header footer added

---
+++ app/(website)/layout.tsx
@@ -0,0 +1,18 @@
+import { Footer } from "../components/layout/Footer";
+import { Header } from "../components/layout/Header";
+
+export default function WebsiteLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className='flex flex-col min-h-screen' >
+      <Header />
+        <main className="flex-1">
+          {children}
+        </main>
+      <Footer />
+    </div>
+  );
+}
---
+++ app/components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => { 
+	return (
+		<div className="w-full h-16 bg-gray-400 flex items-center justify-center font-semibold px-5">
+			Footer
+		</div>
+	)
+}
